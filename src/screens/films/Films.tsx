f47d84a9b48d6d0aac28a293c612b44ce5f31169
Andrey <nepein.dev.aquadelivery@gmail.com>
2021-09-25 17:24:59 +0300

    refactor: removed unnecessary imports

--- src/screens/films/Films.tsx
+++ src/screens/films/Films.tsx
@@ -1,14 +1,9 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { FlatList, ListRenderItemInfo } from 'react-native';
 
 import { compose } from '@reduxjs/toolkit';
-import styled, { useTheme, css } from 'styled-components/native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import color from 'color';
+import styled, { useTheme } from 'styled-components/native';
 
-import Card from '../../ui/Card';
-import Pressable from '../../ui/Pressable';
-import Typography from '../../ui/Typography';
 import withReduxProvider from '../../store/withReduxProvider';
 import withThemeProvider from '../../theme/withThemeProvider';
 import { Film, useGetFilmsQuery } from '../../services/films';
@@ -18,13 +13,12 @@ import FilmCard from './FilmCard';
 const ItemSeparator = styled(Spacer).attrs(props => ({ height: props.theme.indents.margin * (3 / 4) }))``;
 
 const extractFilmId = (film: Film) => film.id;
+const renderItem = ({ item }: ListRenderItemInfo<Film>) => <FilmCard film={item} />;
 
 const Films = () => {
     const { data = [] } = useGetFilmsQuery();
 
     const theme = useTheme();
-
-    const renderItem = useCallback(({ item }: ListRenderItemInfo<Film>) => <FilmCard film={item} />, []);
 
     return (
         <FlatList
@@ -32,9 +26,9 @@ const Films = () => {
             keyExtractor={extractFilmId}
             renderItem={renderItem}
             contentContainerStyle={{ padding: theme.indents.padding }}
-            ItemSeparatorComponent={() => <ItemSeparator />}
+            ItemSeparatorComponent={ItemSeparator}
         />
     );
 };
 
-export default compose(withReduxProvider, withThemeProvider)(Films) as Films;
+export default compose(withReduxProvider, withThemeProvider)(Films) as typeof Films;
